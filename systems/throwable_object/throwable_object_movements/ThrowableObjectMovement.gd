7ba38b6941a76dad9a41ead16d73c32e6f954737
Josh Anthony <jantho1990@gmail.com>
2024-01-27 13:41:07 -0600

    still figuring out movement mechanics

--- systems/throwable_object/throwable_object_movements/ThrowableObjectMovement.gd
+++ systems/throwable_object/throwable_object_movements/ThrowableObjectMovement.gd
@@ -9,11 +9,23 @@ var _velocity := Vector2.ZERO
 
 func integrate_velocity(velocityOwner: CharacterBody2D) -> void:
   _velocity = Vector2.ZERO
-  _velocity += _get_next_velocity(velocityOwner)
-  velocityOwner.velocity += _velocity
+  if _use_set_velocity():
+    _velocity = _get_next_velocity(velocityOwner)
+    velocityOwner.velocity = _velocity
+  else:
+    _velocity += _get_next_velocity(velocityOwner)
+    velocityOwner.velocity += _velocity
   print('DBG: adding %s velocity' % [_velocity])
 
 
 func _get_next_velocity(_velocityOwner: CharacterBody2D) -> Vector2:
   assert(false, 'ThrowableObjectMovement._get_next_velocity(): Using base class, extend ThrowableObjectMovement and inherit the _get_next_velocity function.')
   return Vector2.ZERO
+
+
+func _set_next_velocity(_velocityOwner: CharacterBody2D) -> void:
+  assert(false, 'ThrowableObjectMovement._set_next_velocity(): Using base class, extend ThrowableObjectMovement and inherit the _set_next_velocity function.')
+
+
+func _use_set_velocity() -> bool:
+  return false
