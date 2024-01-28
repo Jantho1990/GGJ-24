7bc0513e1122d858e466ea0a988f630a5f2e44bd
danraffe <dclark@dev-10.com>
2024-01-27 18:36:38 -0600

    hazard sprites and code added

---
+++ scenes/hazards/slide_me.gd
@@ -0,0 +1,21 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$sprite.play('default')
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func explode():
+	$shape.disabled = true;
+	$sprite.play('explode');
+
+func _on_sprite_animation_finished():
+	if $sprite.animation == 'explode':
+		queue_free();
+	pass # Replace with function body.
