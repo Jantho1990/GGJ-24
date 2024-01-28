59edda040a4858a67eed9412a98d349f7c9f3788
Josh Anthony <jantho1990@gmail.com>
2024-01-28 13:50:11 -0600

    title screen starts the music

---
+++ screens/TitleScreen/TitleScreen.gd
@@ -0,0 +1,11 @@
+extends Screen
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  MusicManager.play('Gameplay')
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+  pass
